f56e7f9b107e97227e87303822bcf98255090f09
BARDAMRI <62547652+BARDAMRI@users.noreply.github.com>
2022-01-02 19:36:36 +0200

    WORK

--- ClientSide.py
+++ ClientSide.py
@@ -1,61 +1,70 @@
+from os import wait
+import select as sel
+
 import socket
 import struct as str
-import selectors
 import sys
+from scapy.all import *
 
 
-sel = selectors.DefaultSelector()
-conn = None
+game_over=False
+answers=0
 
-
-
-
-
-def read(conn, msk):
+def read(conn):
+    global answers
     data = conn.recv(1024)
     if data:
-        print (str.unpack("s", data)[0])
-    else:
-        conn.close()
-    sel.register(sys.stdin, selectors.EVENT_READ, answer)
-
-def answer (std_in,msk):
-    ans = std_in.readline(1)
-    conn.sendall(str.pack("s",ans))
-    sel.register(conn, selectors.EVENT_READ, read)
+        answers=answers+1
+        print(data.decode())
 
 
+def answer(conn):
+    ans = sys.stdin.read(1)
+    conn.sendall(ans.encode())
 
 name = "Avengers\n"
-port = 13117
-address = ("", port)
-print("Client started, listening for offer requests...")
-UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ip_type = input("Choose eth1 or eth2 =>\n")
+HOST= get_if_addr("eth2")
 
-UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-UDPSocket.bind(address)
+def setUdpConn():
+    UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    UDPSocket.bind(('', 13117))
+    return UDPSocket
+
+def setTcpConn(ip,port):
+    print("Received offer from server at" , ip , " , attempting to connect...")
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((ip, port))
+    tcp_socket.sendall(name.encode())
+    return tcp_socket
+
+def getOffers():
+    UDPSock= setUdpConn()
+    mes, add = UDPSock.recvfrom(7)
+    magic_cockie, tp, tcp_port = str.unpack('>IbH',mes)
+    while magic_cockie != 0Xabcddcba or tp != 0X2:
+        mes, add = UDPSock.recvfrom(7)
+        magic_cockie, tp, tcp_port = str.unpack('>IbH',mes)
+    UDPSock.close()
+    return mes,add
 
 
+while True:
+    game_over=False
+    print("\n\nClient started, listening for offer requests...")
+    msg,address= getOffers()
+    magic_cockie, tp, tcp_port = str.unpack('>IbH',msg)
+    if magic_cockie == 0Xabcddcba and tp == 0X2:
+        with setTcpConn(address[0],tcp_port) as TCPConn:
+           while answers<2:
+                readers, _, _ = sel.select([TCPConn, sys.stdin], [], [])
+                for key in readers:
+                    if key is TCPConn:
+                        read(TCPConn)
+                    else:
+                        answer(TCPConn)
 
-if __name__ == '__main__':
-    while True:
-     
-        msg, address = UDPSocket.recvfrom(1024) 
-        
-        print(str.unpack("Ihh", msg))
-        magic_cockie, tp, server_port = str.unpack("Ihh", msg)
-        print(server_port)
-        conn = tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(address)
-        print("here connect1")
-        tcp_socket.bind((address[0],server_port))
-        print("here connect")
-        tcp_socket.sendall(str.pack("h", name.split()))
-        print("here")
-        sel.register(tcp_socket, selectors.EVENT_READ, read)
-        sel.register(sys.stdin, selectors.EVENT_READ, answer)
 
-        events = sel.select()
-        for key, msk in events:
-            call_back = key.data
-            call_back(key.fileobj, msk)
--- serverSide.py
+++ serverSide.py
@@ -1,101 +1,133 @@
 from os import name
+from shutil import Error
 import socket
 import sys
-import struct as str
+import struct as stru
 import time as t
-import _thread
 import threading
 import random
-from scapy.arch import get_if_addr
+from scapy.all import *
 
-lock=threading.Lock()
+lock = threading.Lock()
+done = False
+has_winn = False
+stop_broadcast = False
+q = ["2+2", "3*3", "4-1", "6+2", "1+5", "8-4", "9-4"]
+a = [4, 9, 3, 8, 6, 4, 5]
 
-def send_invites(stop,UDPSocket,data_to_send,Broadcast_address):
-    while(not stop):
-        #wait for a second
+
+def send_invites(UDPSocket, data_to_send, port):
+    global stop_broadcast
+    Broadcast_address = ('<broadcast>', port)
+    while (not stop_broadcast):
+        # wait for a second
         t.sleep(1)
-        #send the broadcast
-        print("server send")
-        UDPSocket.sendto(data_to_send,Broadcast_address)
+        # send the broadcast
+        UDPSocket.sendto(data_to_send, Broadcast_address)
 
-def get_answer(sock:socket,name1,name2,answer,done,has_winner):
-    with sock:
-        sock.timeout(10)
-        try:
-            ans=sock.recv(1024)
-            message="Game over!\nThe correct answer was "+answer+"!\n\nCongratulations to the winner: "
-            with lock:
-                if( not done ):
-                    done=True
-                    if(ans==answer):
-                        message=message+name1
-                        sock.sendall(str.pack("s",message))
-                        has_winner=True
-                    else :
-                        message=message+name2 
-                        sock.sendall(str.pack("s",message))
-                elif not has_winner:
-                        message=message+name1
-                        sock.sendall(str.pack("s",message)) 
-                elif has_winner:
-                        message=message+name2
-                        sock.sendall(str.pack("s",message)) 
-        except sock.timeout as e:
-            sock.sendall(str.pack("s","Oops, the time to answer has left. \nBe faster next time!"))           
 
-send_stop=False
-if __name__ == '__main__':
-    #check in lab the broadcast ip
-    BroadCastIp = get_if_addr('eth2')
+def get_answer(sock: socket, name1, name2, answer):
+    global has_winn, done
+    message = "Game over!\nThe correct answer was " + str(answer) + "!\n\nCongratulations to the winner: "
+    sock.settimeout(10)
+    try:
+        data = sock.recv(1)
+        ans = (data).decode()
+        with lock:
+            if (not done):
+                done = True
+                if (ans == answer):
+                    message = str(message) + str(name1)
+                    sock.sendall(message.encode())
+                    has_winn = True
+                else:
+                    message = str(message) + str(name2)
+                    sock.sendall(message.encode())
+            elif not has_winn:
+                message = str(message) + str(name1)
+                sock.sendall(message.encode())
+                has_winn = True
+            elif has_winn:
+                message = str(message) + str(name2)
+                sock.sendall(message.encode())
+        sock.close()
+    except socket.timeout:
+        if not done and not has_winn:
+            sock.sendall(("Game over!\nThe correct answer was " + str(answer) + " .It was a draw.").encode())
+        else:
+            message = str(message) + str(name2)
+            sock.sendall(message.encode())
+        sock.close()
 
-    port=13117
+
+def SetGameSocket():
+    TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPsock.bind(('', 2098))
+    TCPsock.listen(2)
+    return TCPsock
+
+
+def setBroadcastSock():
     # Create a UDP socket
     UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Bind the socket to the port
-    Broadcast_address = ('<broadcast>', port)
-    address=(BroadCastIp, port)
-    print("Server started, listening on IP address", BroadCastIp)
-    magic_cookie = 0xabcddcba
-    msg_type= 0x2
-   
-    data_to_send=str.pack("Ihh",magic_cookie,msg_type,port)
-    q=["2+2","3*3","4-1","6+2","1+5","8-4","9-4"]
-    a=[4,9,3,8,6,4,5]
-    TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    TCPsock.bind(address)
-    TCPsock.listen(2)
-    with TCPsock:
-        while True:
-            send_stop=False
-            print("open threads")
-            _thread.start_new_thread(send_invites,(send_stop,UDPSocket,data_to_send,Broadcast_address))
-            print("here")
-            conn1, addr1 = TCPsock.accept()
-            print("here")
-            conn2, addr2 = TCPsock.accept()
-            send_stop=True
-            t.sleep(10)
-            name1=str.unpack("s" ,conn1.recv(1024))
-            name2=str.unpack("s" ,conn2.recv(1024))
-            if not name1:
-                    conn1.close()
-                    conn2.close()
-                    continue
-            if not name2:
-                    conn1.close()
-                    conn2.close()
-                    continue
-            name1= name1[0,name1.index('\n')]
-            name2= name2[0,name2.index('\n')]
-            loc= random.randint(0,6)
-            message= "Welcome to Quick Maths.\nPlayer 1: "+name1+"\nPlayer2: "+name2+"\nHow much is "+q[loc]+"?\n"    
-            conn1.sendall(str.pack("s",message)) 
-            conn2.sendall(str.pack("s",message))
-            done=False
-            _thread.start_new_thread(get_answer,(conn1,name1,a[loc],done)) 
-            _thread.start_new_thread(get_answer,(conn2,name2,a[loc],done))
-            #update on starting a new game
-            print("Game over, sending out offer requests...")
+    UDPSocket.bind(('', 13117))
+    return UDPSocket
 
-  
+
+def createMessage():
+    return stru.pack('>IbH', 0Xabcddcba, 0X2, 2098)
+
+
+# check in lab the broadcast ip
+HOST = get_if_addr("eth2")
+print("Server started, listening on IP address ", HOST)
+msg = createMessage()
+UDPSock = setBroadcastSock()
+
+while True:
+    TCPsock = SetGameSocket()
+    stop_broadcast = False
+    broadcaster = threading.Thread(target=send_invites, args=(UDPSock, msg, 13117))
+    broadcaster.start()
+    conn1, addr1 = TCPsock.accept()
+    conn2, addr2 = TCPsock.accept()
+    stop_broadcast = True
+    broadcaster.join()
+    print("Two participants connected. starting game in 10 seconds")
+    t.sleep(3)
+    name1 = conn1.recv(1024).decode()
+    name2 = conn2.recv(1024).decode()
+    if not name1:
+        conn1.close()
+        conn2.close()
+        continue
+    if not name2:
+        conn1.close()
+        conn2.close()
+        continue
+    name1 = name1[:-1]
+    name2 = name2[:-1]
+    loc = random.randint(0, 6)
+    ques = q[loc]
+    ans = a[loc]
+    message = "\n\nWelcome to Quick Maths.\nPlayer 1: " + str(name1) + "\nPlayer2: " + str(
+        name2) + "\nHow much is " + str(ques) + "?\n"
+    conn1.sendall(message.encode())
+    conn2.sendall(message.encode())
+    receiver1 = threading.Thread(target=get_answer, args=(conn1, name1, name2, ans))
+    receiver2 = threading.Thread(target=get_answer, args=(conn2, name2, name1, ans))
+    receiver1.start()
+    receiver2.start()
+    receiver1.join()
+    receiver2.join()
+    conn1.close()
+    conn2.close()
+    TCPsock.close()
+    t.wait(10)
+    # update on starting a new game
+    print("Game over, sending out offer requests...\n\n")
+
